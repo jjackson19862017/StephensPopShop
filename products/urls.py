e4ae289164106dae7075b7e2b02e98266f09db47
Stephen Jackson <jjackson19862017@gmail.com>
2020-04-02 03:29:12 +0100

    CRUD pt 2

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url, include
-from .views import all_products, addproducts, SAO_products
+from .views import all_products, addproducts
 
 
 urlpatterns = [
     url(r"^$", all_products, name="products"),
-    url(r"^SAO/$", SAO_products, name="SAO_products"),
     url(r"^add/$", addproducts, name="addproducts"),
 ]
