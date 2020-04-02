fe593b3cceb96fd6acfbdf7b7ef8fc4a541da5b4
Stephen Jackson <jjackson19862017@gmail.com>
2020-04-02 05:08:49 +0100

    django.core.urlresolvers Travis Fix

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.db.models.signals import post_save
 from django.shortcuts import (HttpResponseRedirect, get_object_or_404,
                               redirect, render)
